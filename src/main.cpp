882e3a4c610c0525373a8bccb475f378e612e2a4
math-araujo <araujo_m@usp.br>
2022-11-25 22:33:16 -0300

    Add basic application structure using producer-consumer

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,135 @@
+#include <algorithm>
+#include <atomic>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
 #include <polyscope/point_cloud.h>
 #include <polyscope/polyscope.h>
+#include <thread>
 #include <vector>
+
+#include "semaphore.hpp"
+
+struct Application
+{
+    const int number_of_points{10000};
+    std::atomic<bool> running{true};
+    std::atomic<bool> animate{false};
+    pbma::Semaphore empty{1};
+    pbma::Semaphore full{0};
+    pbma::Semaphore mutex{1};
+    std::vector<glm::vec3> points;
+    std::vector<float> positions;
+    float displacement{0.0f};
+    int sign{1};
+
+    Application()
+    {
+        // Initialize polyscope
+        polyscope::init();
+        set_polyscope_settings();
+    }
+
+    void set_polyscope_settings()
+    {
+        polyscope::options::automaticallyComputeSceneExtents = false;
+        polyscope::options::ssaaFactor = 2;
+        polyscope::view::farClipRatio = 100.0;
+        polyscope::state::lengthScale = 2.0f;
+        polyscope::options::alwaysRedraw = true;
+        polyscope::state::userCallback = [this]() { this->user_callback(); };
+    }
+
+    void run()
+    {
+        std::thread animation_thread{&Application::main_loop, this};
+
+        polyscope::show(); // This function call blocks the main thread
+
+        /*
+        When the window is closed, the main thread is unblocked.
+        We need to set running to false and release the semaphores
+        to notify the simulation thread to stop.
+        */
+        shutdown();
+        animation_thread.join();
+        std::cout << "Bye!" << std::endl;
+    }
+
+    void main_loop()
+    {
+        while (running.load())
+        {
+            update();
+        }
+    }
+
+    void update()
+    {
+        if (animate.load())
+        {
+            empty.acquire();
+            mutex.acquire();
+            {
+                //
+                if (sign > 0 && displacement > 4.0f)
+                {
+                    sign = -1;
+                }
+                else if (sign < 0 && displacement < 1.0f)
+                {
+                    sign = 1;
+                }
+
+                displacement += (sign)*0.01f;
+                positions.emplace_back(displacement);
+            }
+            mutex.release();
+            full.release();
+        }
+    }
+
+    void user_callback()
+    {
+        ImGui::PushItemWidth(100);
+        if (ImGui::Button("Animate"))
+        {
+            animate.store(true);
+        }
+
+        ImGui::PopItemWidth();
+
+        // Update point cloud
+        points.clear();
+        for (int i = 0; i < number_of_points; ++i)
+        {
+            points.emplace_back(glm::vec3{polyscope::randomUnit(), polyscope::randomUnit(), polyscope::randomUnit()});
+        }
+        auto point_cloud = polyscope::registerPointCloud("Sample Point Cloud", points);
+
+        if (animate.load())
+        {
+            full.acquire();
+            mutex.acquire();
+            point_cloud->setTransform(glm::translate(glm::mat4{1.0f}, glm::vec3{0.0f, displacement, 0.0f}));
+            ImGui::PlotLines("Position over Time", positions.data(),
+                             std::min(positions.size(), std::size_t{1'000'000}));
+            mutex.release();
+            empty.release();
+        }
+    }
+
+    void shutdown()
+    {
+        running.store(false);
+        mutex.release();
+        empty.release();
+    }
+};
 
 int main()
 {
-    std::cout << "Hello PBMA!" << std::endl;
+    Application app{};
+    app.run();
+    return 0;
 }
